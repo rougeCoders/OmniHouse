35bed27dc438cb61e1a20ce2362940b46f248eb9
Gaurav Arora <gaurav.arora1@publicissapient.com>
2020-12-06 21:37:52 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const App = () => {
+  return(
+    <View style={{marginTop:50}} >
+      <Text>
+        Hello
+      </Text>
+    </View>
+  )
+}
+
+export default App;
